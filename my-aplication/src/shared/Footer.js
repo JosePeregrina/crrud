65cd1239c34b920f85a9d72d3bbcec213244b22c
Grillo <jperegrina2@ucol.mx>
2019-10-26 12:33:07 -0500

    The Footer and header component were created and imported to the App.js

---
+++ my-aplication/src/shared/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer className="bg-secondary">
+            <small>Footer component</small>
+        </footer>
+    )
+}
+export default Footer;
